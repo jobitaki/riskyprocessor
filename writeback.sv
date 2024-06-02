40a3ca1b18cd8d9cfbdf9ffe0ad2d685bfee789c
Jaehyun Lim <jaehyunrlim@gmail.com>
2024-06-02 23:38:45 +0900

    Fixed LB byte indexing

--- writeback.sv
+++ writeback.sv
@@ -1,8 +1,8 @@
 `default_nettype none
 
 module writeback (
-    input  logic clk,
-    input  logic rst_n,
+    input  logic        clk,
+    input  logic        rst_n,
     input  logic [31:0] instr_i,
     input  logic [31:0] alu_result_i,
     input  logic [31:0] data_i,
@@ -19,13 +19,18 @@ module writeback (
       sel_rd_o <= '0;
       we_o <= '0;
       data_o <= '0;
-    end
-    else begin
+    end else begin
       casez (instr_i)
         LB: begin
           sel_rd_o <= instr_i[11:7];
           we_o     <= 1'b1;
-          data_o   <= {{24{data_i[7]}}, data_i[7:0]};
+          case (alu_result_i[1:0])
+            2'b00:   data_o <= {{24{data_i[7]}}, data_i[7:0]};
+            2'b01:   data_o <= {{24{data_i[15]}}, data_i[15:8]};
+            2'b10:   data_o <= {{24{data_i[23]}}, data_i[23:16]};
+            2'b11:   data_o <= {{24{data_i[31]}}, data_i[31:24]};
+            default: data_o <= '0;
+          endcase
         end
 
         default: begin
